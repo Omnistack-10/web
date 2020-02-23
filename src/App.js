2f66ba52ae15a7538b722f8c43c70873a1bd55b5
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-23 11:05:29 -0300

    ajustando form para entradas de usuario

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ function App() {
   // com as alterações 
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+  const [github_username, setGithub_username] = useState('');
+  const [techs, setTechs] = useState('');
 
   // useEffect serve disparar uma função, toda vez que uma informação alterar
   // Se o 2 parametro (que é um vetor), estiver vazio, a função irá executar 1 vez
@@ -52,23 +54,49 @@ function App() {
 
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}
+              onChange={e => setGithub_username(e.target.value)}  
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+              name="techs" 
+              id="techs" 
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">latitude</label>
-              <input type="number" name="latitude" id="latitude" required value={latitude} />
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">latitude</label>
-              <input type="number" name="longitude" id="longitude" required value={longitude} />
+              <input 
+                type="number"
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude} 
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
