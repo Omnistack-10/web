1fe850c12160aa3143552a09698b8009a7166f1e
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-23 10:03:09 -0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './global.css';
+import './App.css';
+import './Sidebar.css'
 
 // Componente:  Bloco isolado de HTML, CSS e JS, 
 //              o qual não interfere no restante da aplicação
@@ -12,11 +14,41 @@ import './global.css';
 
 // Desinstruturação: Pegar um vetor e quebra-lo em variáveis
 
-function App()  {
+function App() {
   return (
     <div id="app">
-      <aside></aside>
-      <main></main>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">latitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
     </div>
   );
 }
